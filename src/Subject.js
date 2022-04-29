4c7cf9606fa7e7a615e3badf94aa26e9ace4b536
양성룡 <act4ysr@gmail.com>
2022-04-29 14:37:37 +0900

    subject 컴포넌트 추가 변경사항 확인을 위해 css 제거

---
+++ src/Subject.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Component } from 'react';
+
+class Subject extends Component {
+  render(){
+    return (
+        <header>
+            <h1>Web</h1>
+            world wide web!
+      </header>
+    );
+  }
+}
+
+export default Subject;
