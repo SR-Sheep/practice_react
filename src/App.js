7edc870fcf891ae5d6823b474ae0bb3f41d82f79
양성룡 <act4ysr@gmail.com>
2022-04-29 19:08:41 +0900

    state 사용

--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ import Toc from './Toc';
 import Content from './Content';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      subject:{title:"WEB",sub:'World'},
+      content:[
+        {id:1, title : 'HTML', desc:'HTML is for infomation'},
+        {id:2, title : 'CSS', desc:'CSS is for design'},
+        {id:3, title : 'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
+
   render(){
     return (
       <div className="App">
-        <Subject title = "동물" sub="고양이"/>
-        <Toc/>
+        <Subject title = {this.state.subject.title} sub={this.state.subject.sub}/>
+        <Toc data={this.state.content}/>
         <Content/>
       </div>
     );
