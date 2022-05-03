52d3fdbca55f4d41ec1005a7ca1631c89ab07e6c
양성룡 <act4ysr@gmail.com>
2022-05-03 17:25:49 +0900

    useRef 사용한 DOM 선택

--- src/Func.js
+++ src/Func.js
@@ -10,10 +10,13 @@ function Func(){
     //값 추출
     const {name,nickname} = inputs;
 
+    //ref 선언
+    const nameInput = useRef();
+
     //입력값 출력
     const onChange = (e)=>{
         const {value,name} = e.target; //타겟의 name과 value 가져오기
-        console.log(value + " " +name);
+        // console.log(value + " " +name);
         setInputs({
             ...inputs, //state의 inputs 객체 복사
             [name]:value //name => value로 변경
@@ -24,12 +27,14 @@ function Func(){
         setInputs({
             name:'',
             nickname:''
-        })
+        });
+        //리셋 후 커서 위치 조정
+        nameInput.current.focus();
     }
 
     return (
         <div className="func">
-            <input name="name" placeholder="이름" onChange={onChange} value={name} />
+            <input name="name" placeholder="이름" onChange={onChange} value={name} ref={nameInput} />
             <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
             <div className="print">
                 <span>이름 : {name}</span>
